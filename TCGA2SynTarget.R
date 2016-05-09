2e7fd237173a79cb49324cfa4bfebd48d53179e3
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-05-09 10:20:15 -0400

    Fix load_data and summarize_data functions

--- TCGA2SynTarget.R
+++ TCGA2SynTarget.R
@@ -4,14 +4,15 @@ library(dplyr)
 DIR = "/Users/mikhail/Documents/Data/GenomeRunner/TCGAsurvival/data" # Path where the downloaded data is stored
 
 cancer = "BRCA" # Cancer types: http://www.liuzlab.org/TCGA2STAT/CancerDataChecklist.pdf
+cancer = "LIHC" # Liver hepatocellular carcinoma
 data.type = "RNASeq" # Data types: http://www.liuzlab.org/TCGA2STAT/DataPlatforms.pdf
 type = "RPKM" # Expression types: http://www.liuzlab.org/TCGA2STAT/DataPlatforms.pdf
 # Clinical values: http://www.liuzlab.org/TCGA2STAT/ClinicalVariables.pdf
 
 # A function to load TCGA data, from remote repository, or a local R object
-load_data <- function(disease = cancer, data.type = data.type, type = type, DIR = DIR) {
+load_data <- function(disease = cancer, data.type = data.type, type = type, DIR = DIR, force_reload = FALSE) {
   FILE = paste0(DIR, "/mtx_", disease, ".rda") # R object with data
-  if (file.exists(FILE)) {
+  if (all(file.exists(FILE), !(force_reload))) {
     # If the data has been previously saved, load it
     load(file = FILE)
   } else {
@@ -19,9 +20,25 @@ load_data <- function(disease = cancer, data.type = data.type, type = type, DIR 
     mtx <- getTCGA(disease = cancer, data.type = data.type, type = type, clinical = TRUE)
     save(file = FILE, list = c("mtx")) # Save it
   }
+  return(mtx)
 }
 
-load_data(disease = cancer, data.type = data.type, type = type, DIR = DIR)
+summarize_data <- function(mtx = mtx) {
+  # Data exploration
+  print(paste0("Dimensions of expression matrix, genex X patients: ", paste(dim(mtx$dat), collapse = " ")))
+  print(paste0("Dimensions of clinical matrix, patients X parameters: ", paste(dim(mtx$clinical), collapse = " ")))
+  print(paste0("Dimensions of merged matrix, patients X parameters + genes: ", paste(dim(mtx$merged.dat), collapse = " ")))
+  print("Head of the merged matrix")
+  mtx$merged.dat[1:5, 1:10]
+  print("Head of the clinical matrix")
+  mtx$clinical[1:5, 1:10]
+}
+
+
+mtx <- load_data(disease = cancer, data.type = data.type, type = type, DIR = DIR, force_reload = FALSE)
+
+
+
 
 # Data exploration
 # dim(mtx$dat)
