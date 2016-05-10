0741f6b4f5de3045cd2004b40f7028c4f3fca844
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-05-10 09:15:34 -0400

    Update example to filter low expressed genes before the analysis

--- Supplemental_R_script_1.R
+++ Supplemental_R_script_1.R
@@ -47,8 +47,12 @@
 #
 # Example 4:
 # # Prepare expression data
-# expr <- mtx$merged.dat[ ,c(1, 4:ncol(mtx$merged.dat))] 
-# colnames(expr)[1] <- "AffyID"
+# expr <- mtx$merged.dat[ , 4:ncol(mtx$merged.dat)] %>% as.matrix
+# # Filter out low expressed genes
+# # Should be more than 90% of non-zero values
+# ff <- genefilter::pOverA(p = 0.9, A = 0, na.rm = TRUE) 
+# expr <- expr[, apply(expr, 2, ff)] 
+# expr <- data.frame(AffyID = mtx$merged.dat$bcr, expr, stringsAsFactors = FALSE)
 # # Prepare clinical data
 # clin <- mtx$merged.dat[, 1:3]
 # colnames(clin)[1] <- "AffyID"
