e2c5157c30e8cc81952f332891fda722e20d0c18
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-08-13 09:43:10 -0400

    Reorganize analyses

--- survival.R
+++ survival.R
@@ -18,30 +18,19 @@ selected_genes = c("BCL2L11", "MYC", "MYCN") # LUAD
 selected_genes = c("LAPTM4B", "PIP5K1C") # HNSC
 selected_genes = c("MTDH", "SND1") # LIHC
 selected_genes = c("NF1") # LIHC
+selected_genes = c("ANLN") # PAN
 selected_genes = c("TAF2") # LIHC
 selected_genes = c("CPEB2") # BRCA
 selected_genes = c("PECAM1", "S1PR1", "SPNS2", "TEK", "TIE1"); cancer_RNASeq2 = c("PAAD")
 
-### Correlation analysis, selected genes vs. all others, selected cancers, no clinical annotations
-print(paste("Number of patients in", cancer, "cancer:", nrow(expr)))
-cor.corr <- apply(expr, 2, function(x) Hmisc::rcorr(as.numeric(x), as.numeric(expr[, which(colnames(expr) == selected_genes)]))[[1]][1, 2])
-cor.pval <- apply(expr, 2, function(x) Hmisc::rcorr(as.numeric(x), as.numeric(expr[, which(colnames(expr) == selected_genes)]))[[3]][1, 2])
-cor.combined <- data.frame(genes = names(cor.corr), pearson = cor.corr, pval = cor.pval)
-openxlsx::write.xlsx(cor.combined[ order(cor.combined$pearson, decreasing = TRUE), ], paste0("results/CORR_", cancer, "_", selected_genes, ".xlsx"))
-
-### Selected genes, selected cancers, no clinical annotations
-### Run survival analysis for selected genes
+### Analysis 1: Selected genes, selected cancers, no clinical annotations
 kmplot(expr, clin, event_index=2, time_index=3,  affyid = selected_genes, auto_cutoff="true", transform_to_log2 = TRUE, cancer_type = cancer)
 
 ### All genes, selected cancers, no clinical annotations
-### Run survival analysis for all genes
 kmplot(expr, clin, event_index=2, time_index=3,  affyid = "", auto_cutoff="true", transform_to_log2 = TRUE)
 
-### Selected genes, all (or selected) cancers, no clinical annotations
-### Run survival analysis for selected genes in all cancers
-# All cancers with RNASeq2 data
-cancer_RNASeq2 = c("ACC", "BLCA", "BRCA", "CESC", "CHOL", "COAD", "COADREAD", "DLBC", "ESCA", "GBM", "GBMLGG", "HNSC", "KICH", "KIPAN", "KIRC", "KIRP", "LGG", "LIHC", "LUAD", "LUSC", "MESO", "OV", "PAAD", "PCPG", "PRAD", "READ", "SARC", "SKCM", "STAD", "TGCT", "THCA", "THYM", "UCEC", "UCS"); data.type = "RNASeq2"; type = "" 
-#  "LAML" - something wrong with merged data
+
+### Analysis 2: Selected genes, all (or selected) cancers, no clinical annotations
 for (cancer_type in cancer_RNASeq2) {
   print(paste0("Processing cancer ", cancer_type))
   mtx <- load_data(disease = cancer_type, data.type = data.type, type = type, data_dir = data_dir, force_reload = FALSE)
@@ -52,7 +41,6 @@ for (cancer_type in cancer_RNASeq2) {
   ff <- genefilter::pOverA(p = 0.9, A = 0, na.rm = TRUE) 
   expr <- expr[, apply(expr, 2, ff)] 
   expr <- data.frame(AffyID = mtx$merged.dat$bcr, expr, stringsAsFactors = FALSE)
-  
   # Prepare clinical data
   clin <- mtx$merged.dat[, 1:3]
   colnames(clin)[1] <- "AffyID"
@@ -66,11 +54,9 @@ library(ggplot2)
 mtx <- read.table("res/global_stats.txt", sep = "\t", header = TRUE, stringsAsFactors = FALSE, fill = TRUE)
 # Add -log10-transformed p-value
 mtx <- mtx %>% mutate(log10.pval = -1 * log10(p.value))
-# Specify gene name
-gene <- "CPEB2"
 # Print into PDF
-pdf(paste0("Figures/", gene, "_all_TCGA_cancers.pdf"))
-mtx %>% subset(Gene == gene) %>% 
+pdf(paste0("res/", selected_genes, "_all_TCGA_cancers.pdf"))
+mtx %>% subset(Gene == selected_genes) %>% 
   ggplot(aes(x = factor(Cancer, levels(factor(Cancer))[order(log10.pval)]), y = log10.pval)) + 
   geom_bar(stat = "identity") + 
   theme(legend.position="none") +
@@ -79,11 +65,58 @@ mtx %>% subset(Gene == gene) %>%
 dev.off()
 
 
+### Analysis 3: Selected genes, all (or, selected) cancers, all unique categories
+for (cancer_type in cancer_RNASeq2) {
+  print(paste0("Processing cancer ", cancer_type))
+  mtx <- load_data(disease = cancer_type, data.type = data.type, type = type, data_dir = data_dir, force_reload = FALSE)
+  clinical_annotations <- summarize_data(mtx = mtx)
+  # Prepare expression data
+  expr <- mtx$merged.dat[ , 4:ncol(mtx$merged.dat)] %>% as.matrix
+  # Filter out low expressed genes
+  # Should be more than 90% of non-zero values
+  ff <- genefilter::pOverA(p = 0.9, A = 0, na.rm = TRUE) 
+  expr <- expr[, apply(expr, 2, ff)] 
+  expr <- data.frame(AffyID = mtx$merged.dat$bcr, expr, stringsAsFactors = FALSE)
+  # Prepare clinical data
+  clin <- mtx$merged.dat[, 1:3]
+  colnames(clin)[1] <- "AffyID"
+  # Full clinical information
+  clin_full <- mtx$clinical
+  
+  # For each clinical annotation
+  for (annotation in clinical_annotations) { 
+    # Get the number of patients per category in the current annotation
+    annotations <- table(clin_full[, annotation], useNA = "no") 
+    # How many categories in the current annotation
+    num_of_annot_categories <- length(annotations) 
+    # How many categories to select at one time
+    for (num_of_selected_categories in 1:num_of_annot_categories) {
+      # Select patients annotated with this categories
+      patients <- rownames(clin_full)[ clin_full[, annotation] %in% names(annotations)[ num_of_selected_categories] ]
+      # Get their index in the clin and expr matrixes
+      index_patients <- which(clin$AffyID %in% patients)
+      # If the number of patients annotated with the combination of categories is large enough, proceed
+      if (length(index_patients) > 40) {
+        print(paste("Processing annotation:", annotation, 
+                    ", categories:", names(annotations)[ num_of_selected_categories],
+                    ", number of patients:", length(index_patients)))
+        # Get a subset of clinical information for these patients
+        clin_selected <- clin[ index_patients, ]
+        # Get a subset of expression information for these patients
+        expr_selected <- expr[ index_patients, ]
+        # For this subset of expression, filter out low expressed genes
+        index_genes <- apply(expr_selected %>% dplyr::select(-AffyID), 2, ff) # index of expression values to keep
+        expr_selected <- cbind(expr_selected$AffyID, dplyr::select(expr_selected, -AffyID)[, index_genes]) # patient IDs and expression values to keep
+        # Perform actual survival analysis
+        kmplot(expr_selected, clin_selected, event_index=2, time_index=3,  affyid = selected_genes, auto_cutoff="true", transform_to_log2 = TRUE, cancer_type = paste(c(cancer_type, annotation, names(annotations)[ num_of_selected_categories] ), collapse = "-"))
+      }
+    }
+  }
+}
+
 
 ### Selected genes, selected cancers, all combinations of clinical annotations
-### Run survival analysis for selected genes, in selected cancer, across all combinations of categories in each clinical annotation
-# Full clinical information
-clin_full <- mtx$clinical
+clin_full <- mtx$clinical # Full clinical information
 # For each clinical annotation
 for (annotation in clinical_annotations) { 
   # Get the number of patients per category in the current annotation
@@ -118,61 +151,5 @@ for (annotation in clinical_annotations) {
     }
   }
 }
-
-### Selected genes, all cancers, all unique categories
-### Run survival analysis for selected genes, in all cancer, across all unique categories in each clinical annotation
-for (cancer_type in cancer_RNASeq2) {
-  print(paste0("Processing cancer ", cancer_type))
-  mtx <- load_data(disease = cancer_type, data.type = data.type, type = type, data_dir = data_dir, force_reload = FALSE)
-  clinical_annotations <- summarize_data(mtx = mtx)
-  # Prepare expression data
-  expr <- mtx$merged.dat[ , 4:ncol(mtx$merged.dat)] %>% as.matrix
-  # Filter out low expressed genes
-  # Should be more than 90% of non-zero values
-  ff <- genefilter::pOverA(p = 0.9, A = 0, na.rm = TRUE) 
-  expr <- expr[, apply(expr, 2, ff)] 
-  expr <- data.frame(AffyID = mtx$merged.dat$bcr, expr, stringsAsFactors = FALSE)
-  
-  # Prepare clinical data
-  clin <- mtx$merged.dat[, 1:3]
-  colnames(clin)[1] <- "AffyID"
-  # Full clinical information
-  clin_full <- mtx$clinical
-  
-  # For each clinical annotation
-  for (annotation in clinical_annotations) { 
-    # Get the number of patients per category in the current annotation
-    annotations <- table(clin_full[, annotation], useNA = "no") 
-    # How many categories in the current annotation
-    num_of_annot_categories <- length(annotations) 
-    # How many categories to select at one time
-    for (num_of_selected_categories in 1:num_of_annot_categories) {
-      # Select patients annotated with this categories
-      patients <- rownames(clin_full)[ clin_full[, annotation] %in% names(annotations)[ num_of_selected_categories] ]
-      # Get their index in the clin and expr matrixes
-      index_patients <- which(clin$AffyID %in% patients)
-      # If the number of patients annotated with the combination of categories is large enough, proceed
-      if (length(index_patients) > 40) {
-        print(paste("Processing annotation:", annotation, 
-                    ", categories:", names(annotations)[ num_of_selected_categories],
-                    ", number of patients:", length(index_patients)))
-        # Get a subset of clinical information for these patients
-        clin_selected <- clin[ index_patients, ]
-        # Get a subset of expression information for these patients
-        expr_selected <- expr[ index_patients, ]
-        # For this subset of expression, filter out low expressed genes
-        index_genes <- apply(expr_selected %>% dplyr::select(-AffyID), 2, ff) # index of expression values to keep
-        expr_selected <- cbind(expr_selected$AffyID, select(expr_selected, -AffyID)[, index_genes]) # patient IDs and expression values to keep
-        # Perform actual survival analysis
-        kmplot(expr_selected, clin_selected, event_index=2, time_index=3,  affyid = selected_genes, auto_cutoff="true", transform_to_log2 = TRUE, cancer_type = paste(c(cancer_type, annotation, names(annotations)[ num_of_selected_categories] ), collapse = "-"))
-      }
-    }
-  }
-}
-
-
-
-
-
 
 
