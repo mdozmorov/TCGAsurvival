45b8849ffa18d8a62892786650c51a4140790c70
Mikhail Dozmorov <mikhail.dozmorov@gmail.com>
2016-09-06 15:03:02 -0400

    Move the code into TCGA_DEGs

--- pathview.R
+++
@@ -1,7 +0,0 @@
-library(pathview)
-library(openxlsx)
-degs <- read.xlsx("PAAD_CD31_hi51_lo49_DEGs.xlsx", cols = c(1, 2))
-degs.genes <- degs$logFC
-names(degs.genes) <- degs$Gene
-
-pv.out <- pathview(gene.data = degs.genes, pathway.id = "04514", species = "hsa", gene.idtype = "SYMBOL", gene.annotpkg = "org.Hs.eg.db", out.suffix = "CD31")
